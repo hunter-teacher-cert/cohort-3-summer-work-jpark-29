452c26e17494916888b2ef73cfe621ba67e481e5
jpark-29 <100715306+jpark-29@users.noreply.github.com>
2022-06-25 20:09:28 +0000

    I created, compiled, and ran the Guess file. It ran successfully.

--- pre02/Celsius.java
+++ pre02/Celsius.java
@@ -1,6 +1,5 @@
 //Write a program that converts a temperature from Celsius to Fahrenheit. It should (1) prompt the user for input, (2) read a double value from the keyboard, (3) calculate the result, and (4) format the output to one decimal place.
 
-import java.io.*;
 import java.util.Scanner;
 
 public class Celsius{
@@ -8,9 +7,12 @@ public class Celsius{
         double cel;
         double fahren;
         Scanner in = new Scanner(System.in);
-
+        
+        //prompt the user for input
         System.out.print("Enter a temperature in Celsius: ");
         cel = in.nextDouble();
+        
+        //calculate the reselt and format the output to once decimal place 
         fahren = cel*(9.0/5.0) + 32.0;  
         System.out.printf("%.1f C = %.1f F", cel, fahren);
 
